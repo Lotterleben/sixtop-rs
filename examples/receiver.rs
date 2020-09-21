b17c580166e30bfd97fea3e1d87e589dd06854c1
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2020-09-21 14:31:35 +0200

    make sender receive the response; clean up tx/rx a bit

--- examples/receiver.rs
+++ examples/receiver.rs
@@ -1,37 +1,14 @@
 // todo: how do I summarize the io imports (I'm assuming if I do `use std::io::{BufRead, BufReader, Write}`,
 // I will overwrite the default Result?)
 use std::io;
-use std::io::BufRead;
-use std::io::BufReader;
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
-use sixtop_rs::types::{Msg, Response, ReturnCode, SixtopMsg};
+use sixtop_rs::{handle_msg};
+use sixtop_rs::types::{SixtopMsg};
 use sixtop_rs::msg_reader::{deserialize_message};
 use sixtop_rs::msg_builder::{serialize_response};
 
 const IP_AND_PORT: &str = "127.0.0.1:8080";
-
-// dummy handling for now
-// returns an answer to be sent if necessary
-fn dummy_handle_msg(msg: SixtopMsg) -> Result<Option<SixtopMsg>, ()>{
-    match msg {
-        SixtopMsg::RequestMsg(request) => {
-            // dummy: pick first two cells and accept
-            let mut response = Response::new();
-            response.header.code = ReturnCode::RC_SUCCESS as u8;
-            response.header.seqnum = request.header.seqnum; // TODO store and handle seqnum etc
-
-            // just choose the first two cells. obvs missing coherence check etc
-            for index in 0..request.num_cells {
-                response.cell_list.push(*request.cell_list.get(index as usize).unwrap());
-            }
-
-            Ok(Some(SixtopMsg::ResponseMsg(response)))
-        },
-        SixtopMsg::ResponseMsg(response) => { unimplemented!() },
-        _ => { unimplemented!() }
-    }
-}
 
 fn handle_client_connection(mut stream: TcpStream) -> Result<(), io::Error> {
     println!(
@@ -39,22 +16,20 @@ fn handle_client_connection(mut stream: TcpStream) -> Result<(), io::Error> {
         stream.local_addr().unwrap()
     );
 
-    let mut buffer = Vec::new();
     let mut stream_reader = BufReader::new(stream.try_clone().unwrap());
 
     // todo loop here
-    // \n is a hacky way to read into vec instead of st bc I can*t be bothered rn
-    stream_reader.read_until(b'\n', &mut buffer).unwrap();
-
+    let buffer = stream_reader.fill_buf()?.to_vec();
     let msg = deserialize_message(buffer).expect("unable to parse message");
     println!("received: {:#?}", msg);
-    let result = dummy_handle_msg(msg).unwrap();
+
+    let result = handle_msg(msg).unwrap();
     if let Some(response) = result {
         match response {
             SixtopMsg::ResponseMsg(response) => {
                 println!("answered: {:#?}", response);
                 let data = serialize_response(response).unwrap();
-                stream.write(data.as_slice());
+                stream.write(data.as_slice())?;
              },
             _ => { unimplemented!() }
         }
--- examples/sender.rs
+++ examples/sender.rs
@@ -1,12 +1,14 @@
 extern crate sixtop_rs;
+use std::io::BufRead;
+use std::io::BufReader;
 
-use std::io::Write;
+use std::io::{Write};
 use std::net::{TcpStream};
 
-use sixtop_rs::types::{MsgType, Msg,
-                       Cell,
+use sixtop_rs::types::{Msg, Cell,
                        Request, RequestType};
 use sixtop_rs::msg_builder::{serialize_request};
+use sixtop_rs::msg_reader::{deserialize_message};
 
 const SERVER_ADDR: &str = "127.0.0.1:8080";
 
@@ -29,11 +31,20 @@ fn build_msg() -> Vec<u8> {
 
 fn main() {
     let mut stream = TcpStream::connect(SERVER_ADDR).unwrap();
-    let data = build_msg();
-    match stream.write(data.as_slice()) {
+    let mut stream_reader = BufReader::new(stream.try_clone().unwrap());
+
+    // send dummy request
+    let request = build_msg();
+    match stream.write(request.as_slice()) {
         Ok(numbytes) => {
             println!("wrote {} bytes", numbytes)
         }
         Err(error) => { println!("whoops. err {:?}", error)}
     }
+    stream.flush().unwrap();
+
+    // receive and parse answer
+    let buffer = stream_reader.fill_buf().unwrap().to_vec();
+    let response = deserialize_message(buffer).expect("unable to parse message");
+    println!("received: {:#?}", response);
 }
